3152fa86065f4166fb90e07928edf4129314a0f1
Worro <alessiodelledonne29@gmail.com>
2024-06-16 18:09:19 +0200

    excercise compiled

---
+++ src/main/java/com/example/demo/BasicController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class BasicController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String greetings(){
+       String name = environment.getProperty("myCustomVarTree.devName");
+       String code = environment.getProperty("myCustomVarTree.authCode");
+       return "Hello, " + name + "! This is your authentication code: " + code;
+    }
+}
